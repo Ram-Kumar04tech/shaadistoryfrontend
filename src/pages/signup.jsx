65e8176da1d8e9f3bc513742105bdaa77016e2f3
Ram <nrkramofficial@gmail.com>
2025-06-04 07:38:43 +0530

    Update signup.jsx

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -45,7 +45,7 @@ const SignUp = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/register', {
+      const response = await fetch('https://shaadistoryfrontend-j9fm.vercel.app/api/users/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
